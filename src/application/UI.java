790b476ac4a0a6e24ea3f55932962d7e9c35d32f
Gustavo Victorio <gu.victorio3@gmail.com>
2020-06-25 08:51:16 -0300

    Implementing possible moves of king

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,7 +58,7 @@ public class UI {
 		System.out.println("  a b c d e f g h");
 
 	}
-	
+
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -68,7 +68,6 @@ public class UI {
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
-
 	}
 
 	private static void printPiece(ChessPiece piece, boolean background) {
